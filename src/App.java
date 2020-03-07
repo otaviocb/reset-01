140bc37eeadc01e0ac3989353468a923fe88dbfd
Otavio <otaviocb@yahoo.com.br>
2020-03-07 12:13:16 -0300

    removendo outras mensagens

--- src/App.java
+++ src/App.java
@@ -1,9 +1,6 @@
 public class App {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("Outras coisas");
-        System.out.println("Otávio");
-        System.out.println("Hello World?");
-        System.out.println("Hello World!!!!");
+
     }
 }
