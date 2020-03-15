e44a17e1913718c32f45c4f7390709b01d68fb59
Otavio <otaviocb@yahoo.com.br>
2020-03-15 15:48:37 -0300

    tema 1 com extras

---
+++ Aula02/exercicios-aula-2/src/ENUM/Compromisso.java
@@ -0,0 +1,20 @@
+package ENUM;
+
+public enum Compromisso {
+    REUNIAO,
+    CONSULTA_MEDICA,
+    ANIVERSARIO,
+    FESTA;
+
+    private static Cor cor;
+
+    private static int prioridade;
+
+    private static String descricao;
+
+    Compromisso(Cor cor, int prioridade){
+        this.cor = cor;
+        this.prioridade;
+        this.descricao;
+    }
+}
